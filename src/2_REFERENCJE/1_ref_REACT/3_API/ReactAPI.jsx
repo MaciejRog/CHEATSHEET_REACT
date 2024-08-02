f808e9dc2dfa40faea3ce0ad6c97d496b561033d
Maciej <maciek333programowanie@gmail.com>
2024-08-02 11:37:41 +0200

    add base structure for react apis infos

--- src/2_REFERENCJE/1_ref_REACT/3_API/ReactAPI.jsx
+++ src/2_REFERENCJE/1_ref_REACT/3_API/ReactAPI.jsx
@@ -1,5 +1,84 @@
 function ReactAPI() {
-  return <div></div>;
+  return (
+    <div>
+      <ReactAPIAct />
+      <ReactAPICache />
+      <ReactAPICreateContext />
+      <ReactAPIForwardRef />
+      <ReactAPILazy />
+      <ReactAPIMemo />
+      <ReactAPIStartTransition />
+      <ReactAPIUse />
+    </div>
+  );
 }
 
 export default ReactAPI;
+
+// #################################
+// #### act | -> pomoc przy testach (pomaga załadować trwające aktualizacje React )
+// VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
+
+function ReactAPIAct() {
+  return <div></div>;
+}
+
+// #################################
+// ####
+// VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
+
+function ReactAPICache() {
+  return <div></div>;
+}
+
+// #################################
+// ####
+// VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
+
+function ReactAPICreateContext() {
+  return <div></div>;
+}
+
+// #################################
+// ####
+// VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
+
+function ReactAPIForwardRef() {
+  return <div></div>;
+}
+
+// #################################
+// ####
+// VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
+
+function ReactAPILazy() {
+  return <div></div>;
+}
+
+// #################################
+// ####
+// VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
+
+function ReactAPIMemo() {
+  return <div></div>;
+}
+
+// #################################
+// ####
+// VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
+
+function ReactAPIStartTransition() {
+  return <div></div>;
+}
+
+// #################################
+// ####
+// VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
+
+function ReactAPIUse() {
+  return <div></div>;
+}
+
+// #################################
+// ####
+// VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
